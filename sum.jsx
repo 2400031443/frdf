9d73f491c26a212d5224ea8720f8e31b02eaddb8
Guru sri charan <2400031443@kluniversity.in>
2025-09-22 11:36:40 +0530

    Add files via upload
    done

---
+++ sum.jsx
@@ -0,0 +1,18 @@
+import{useState}from "react";
+function sum(){
+    let a=10;
+    let b=20;
+    const [XPathResult,setsum]=usestate(0);
+    const handleADD+()=>{
+        setsum(a+b);
+    };
+    return{
+        <div>
+        <h1> the output is :</h1>
+        <div>
+            <button type="button" onclick={handleADD}>ADD</button>
+            <output>{result}</output>
+                    </div>
+        </div>
+    }
+}
